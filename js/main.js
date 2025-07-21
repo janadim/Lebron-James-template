c4b2164ddeb295d28d203bb983d4ea355a83e893
JanaDTitarchuk <Jana.d.titarchuk@gmail.com>
2025-07-21 12:16:48 +0200

    animation and fixes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 "use strict";
-//Global const and variables definitions
+// Global const and variables
 const canvas = document.querySelector(".canvas");
 const canvasText = document.querySelector(".canvasText");
 const canvasPlayer = document.querySelector(".canvasPlayer");
 const canvasPlayerEffect = document.querySelector(".canvasPlayerEffect");
-// const canvasAnimation = document.querySelector(".canvasAnimation");
 const ctx = canvas.getContext("2d");
 const ctx2 = canvasText.getContext("2d");
 const ctx3 = canvasPlayer.getContext("2d");
 const ctx4 = canvasPlayerEffect.getContext("2d");
-// const ctx5 = canvasAnimation.getContext("2d");
 const pathImages = "./images/";
 const typeImages = ".png";
+
 let toggleColorText = 0;
 let togglePlayerOpacity = 1;
+let animationRunning = false;
+let sparkles = [];
+let sparkleCache = [];
+let playerEffectCache = [];
 
-//This array contains the layer information from the photoshop template
+const bgSelector = document.querySelector(".bg");
+const colorSelector = document.querySelector(".color");
+const opacitySelector = document.querySelector(".opacity");
+const animationSelector = document.querySelector(".animation");
+const containerBg = document.querySelector(".container");
+
 const contentTemplate = [
   {
     type: "text",
@@ -197,8 +205,7 @@ const contentTemplate = [
   },
 ];
 
-// Canvas functions definitions
-
+// Load functions
 function loadImage(imObj) {
   var image = new Image();
   image.onload = function () {
@@ -207,6 +214,7 @@ function loadImage(imObj) {
   };
   image.src = imObj.path;
 }
+
 function loadImagePlayer(imObj) {
   var image = new Image();
   image.onload = function () {
@@ -215,26 +223,34 @@ function loadImagePlayer(imObj) {
   };
   image.src = imObj.path;
 }
+
 function loadImagePlayerEffect(imObj) {
   var image = new Image();
   image.onload = function () {
-    ctx4.globalAlpha = imObj.opacity;
-    ctx4.drawImage(image, imObj.dx, imObj.dy);
+    playerEffectCache.push({
+      image: image,
+      dx: imObj.dx,
+      dy: imObj.dy,
+      opacity: imObj.opacity,
+    });
+    // Draw immediately if not animating
+    if (!animationRunning) {
+      ctx4.globalAlpha = imObj.opacity;
+      ctx4.drawImage(image, imObj.dx, imObj.dy);
+    }
   };
   image.src = imObj.path;
 }
 
 function loadText(txObj) {
   ctx2.font = txObj.font;
-  if (toggleColorText == 1) {
-    ctx2.fillStyle = "rgb(253, 204, 36)";
-  } else {
-    ctx2.fillStyle = "white";
-  }
+  ctx2.fillStyle = toggleColorText ? "rgb(253, 204, 36)" : "white";
   ctx2.fillText(txObj.content, txObj.dx, txObj.dy);
 }
 
 function render() {
+  playerEffectCache = [];
+  ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
   for (let i = 0; i < contentTemplate.length; i++) {
     if (contentTemplate[i].type == "image") {
       loadImage(contentTemplate[i]);
@@ -248,101 +264,112 @@ function render() {
   }
 }
 
-// First render on window load
-window.onload = render();
-
-// Handle functions for Form:
-const bgSelector = document.querySelector(".bg");
-const colorSelector = document.querySelector(".color");
-const opacitySelector = document.querySelector(".opacity");
-const animationSelector = document.querySelector(".animation");
-// const containerAnimation = document.querySelector(".containerAnimation");
-const containerBg = document.querySelector(".container");
-
-function handleCheckboxBg() {
-  containerBg.classList.toggle("altBg");
+function reRender() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
+  ctx3.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
+  ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
+  render();
+  drawPlayerEffects();
 }
-bgSelector.addEventListener("change", handleCheckboxBg);
 
-function handleCheckboxColor() {
-  if (colorSelector.checked) {
-    toggleColorText = 1;
-    //Clear first the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
-    ctx3.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
-    ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
-    //render the canvas again
-    render();
-  } else {
-    toggleColorText = 0;
-    //Clear first the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
-    ctx3.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
-    ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
-    //render the canvas again
-    render();
+function createSparkle() {
+  return {
+    x: Math.random() * canvasPlayerEffect.width,
+    y: Math.random() * canvasPlayerEffect.height,
+    radius: Math.random() * 2 + 1,
+    opacity: Math.random() * 0.5 + 0.5,
+    speedY: Math.random() * 0.5 + 0.2,
+  };
+}
+
+function animateSparkles() {
+  if (!animationRunning) return;
+
+  ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
+  drawPlayerEffects();
+
+  sparkles.forEach((sparkle) => {
+    ctx4.beginPath();
+    ctx4.globalAlpha = sparkle.opacity;
+    ctx4.arc(sparkle.x, sparkle.y, sparkle.radius, 0, Math.PI * 2);
+    ctx4.fillStyle = "yellow";
+    ctx4.fill();
+
+    sparkle.y += sparkle.speedY;
+    if (sparkle.y > canvasPlayerEffect.height) {
+      sparkle.y = 0;
+      sparkle.x = Math.random() * canvasPlayerEffect.width;
+    }
+  });
+
+  ctx4.globalAlpha = 1;
+  requestAnimationFrame(animateSparkles);
+}
+
+function drawPlayerEffects() {
+  for (let obj of playerEffectCache) {
+    ctx4.globalAlpha = obj.opacity;
+    ctx4.drawImage(obj.image, obj.dx, obj.dy);
   }
+  ctx4.globalAlpha = 1;
 }
-colorSelector.addEventListener("change", handleCheckboxColor);
 
-function handleCheckboxOpacity() {
-  if (opacitySelector.checked) {
-    togglePlayerOpacity = 0.75;
-    //Clear first the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
-    ctx3.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
-    ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
-    //render the canvas again
-    render();
-  } else {
-    togglePlayerOpacity = 1;
-    //Clear first the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
-    ctx3.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
-    ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
-    //render the canvas again
-    render();
+// State persistence
+function persistCheckboxState(selector, key, callback) {
+  const saved = localStorage.getItem(key);
+  if (saved !== null) {
+    selector.checked = saved === "true";
+    callback();
   }
+
+  selector.addEventListener("change", () => {
+    localStorage.setItem(key, selector.checked);
+    callback();
+  });
 }
-opacitySelector.addEventListener("change", handleCheckboxOpacity);
 
-// function handleCheckboxAnimation() {
-//   animationSelector.classList.toggle("canvasAnimation");
-//   let id = null;
-//   const elem = document.getElementById("canvasAnimation");
-//   let pos = 0;
+persistCheckboxState(bgSelector, "bgSelector", () => {
+  containerBg.classList.toggle("altBg", bgSelector.checked);
+  reRender();
+});
 
-//   if (animationSelector.checked) {
-//     //   clearInterval(id);
-//     //   id = setInterval(frame, 5);
-//     //   function frame() {
-//     //     if (pos == 350) {
-//     //       clearInterval(id);
-//     //     } else {
-//     //       pos++;
-//     //       elem.style.top = pos + "px";
-//     //       elem.style.left = pos + "px";
-//     //     }
-//     //   }
-//     //Clear first the canvas
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
-//     ctx3.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
-//     ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
-//     //render the canvas again
-//     render();
-//   } else {
-//     //Clear first the canvas
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     ctx2.clearRect(0, 0, canvasText.width, canvasText.height);
-//     ctx3.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
-//     ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
-//     //render the canvas again
-//     render();
-//   }
-// }
-// animationSelector.addEventListener("change", handleCheckboxAnimation);
+persistCheckboxState(colorSelector, "colorSelector", () => {
+  toggleColorText = colorSelector.checked ? 1 : 0;
+  reRender();
+});
+
+persistCheckboxState(opacitySelector, "opacitySelector", () => {
+  togglePlayerOpacity = opacitySelector.checked ? 0.75 : 1;
+  reRender();
+});
+
+persistCheckboxState(animationSelector, "animationSelector", () => {
+  if (animationSelector.checked) {
+    startAnimation();
+  } else {
+    stopAnimation();
+  }
+});
+
+// Animation control
+function startAnimation() {
+  animationRunning = true;
+  sparkles = Array.from({ length: 100 }, createSparkle);
+  animateSparkles();
+}
+
+function stopAnimation() {
+  animationRunning = false;
+  ctx4.clearRect(0, 0, canvasPlayerEffect.width, canvasPlayerEffect.height);
+  drawPlayerEffects();
+}
+
+// Initial render
+window.onload = () => {
+  render();
+  if (bgSelector.checked) containerBg.classList.add("altBg");
+  if (colorSelector.checked) toggleColorText = 1;
+  if (opacitySelector.checked) togglePlayerOpacity = 0.75;
+  if (animationSelector.checked) startAnimation();
+};
